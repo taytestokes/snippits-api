d8df9a326946679e12055178d28f20dc6e9993c8
TayteStokes <taytestokes9328@gmail.com>
2021-08-22 10:49:06 -0600

    Adds user login and creates jwt

--- controllers/authorization.js
+++ controllers/authorization.js
@@ -4,20 +4,39 @@ const validator = require('validator')
 
 const User = require('../models/user')
 
-exports.login = (req, res, next) => {
+exports.login = async (req, res, next) => {
   const { email, password } = req.body
 
-  if (!validator.isEmail(email)) {
+  if (!validator.isEmail(email) || !password) {
     return res.status(401).json({
-      message: 'Invalid email, please try again.'
+      message: 'Invalid credentials, please try again.'
     })
   }
 
-  // Hash and compare password sent in req to hashed password stored
+  try {
+    const user = await User.findOne({ email })
+    const hashedPassword = await bcrypt.hash(password, 12)
+    const isValidPassword = await bcrypt.compare(password, hashedPassword)
 
-  res.status(200).json({
-    message: 'Logged in!'
-  })
+    if (user && isValidPassword) {
+      const token = jwt.sign(
+        {
+          email: user.email,
+          userId: user._id.toString()
+        },
+        'supertopsecrettokensecret',
+        { expiresIn: '1h' }
+      )
+
+      return res.status(200).json({
+        message: 'Login Successful!',
+        userId: user._id.toString(),
+        token
+      })
+    }
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 // Register User Into Application
@@ -39,14 +58,11 @@ exports.register = async (req, res, next) => {
 
     const newUser = await user.save()
 
-    console.log(newUser)
-
-    res.status(200).json({
+    return res.status(200).json({
       message: 'User successfully created!',
       user: newUser
     })
   } catch (error) {
-    // Do something with the error
     console.log(error)
   }
 }
